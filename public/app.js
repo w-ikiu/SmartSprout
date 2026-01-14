6ee8cd67ce90a6b328a11c31e256027d07b11577
w-ikiu <chelminska.wiktoria@wp.pl>
2026-01-14 23:37:16 +0100

    pierwsza wersja panelu  logowania i rejestracji

--- public/app.js
+++ public/app.js
@@ -1,6 +1,80 @@
+// pobranie danych z localStorage
+let TOKEN = localStorage.getItem('token');
+let ROLE = localStorage.getItem('role');
+let USERNAME = localStorage.getItem('username');
+
+// jezeli uzytkownik jest zalogowany to pokazujemy aplikacje
+if (TOKEN) {
+    showApp();
+}
+
+// logowanie/rejestracja
+async function auth(action) {
+    // action to 'login' albo 'register'
+    const user = document.getElementById('username').value;
+    const pass = document.getElementById('password').value;
+
+    if (!user || !pass) return alert("Podaj login i hasło!");
+
+    try {
+        const res = await fetch(`/api/${action}`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ username: user, password: pass })
+        });
+
+        const data = await res.json();
+
+        if (!res.ok) {
+            throw new Error(data.error || "Błąd połączenia");
+        }
+
+        if (action === 'register') {
+            alert("Zarejestrowano pomyślnie! Teraz możesz się zalogować.");
+        } else {
+            // przy logowaniu zapisujemy dane do localStorage !!!
+            localStorage.setItem('token', data.token);
+            localStorage.setItem('role', data.role);
+            localStorage.setItem('username', data.username);
+            
+            // aktualizacja zmiennych globalnych
+            TOKEN = data.token;
+            ROLE = data.role;
+            USERNAME = data.username;
+            
+            showApp();
+        }
+    } catch (err) {
+        alert(err.message);
+    }
+}
+
+// wylogowanie
+function logout() {
+    // powiadomienie serwera o wylogowaniu
+    // if (TOKEN) {
+    //     fetch('/api/logout', { 
+    //         method: 'POST', 
+    //         headers: { 'Authorization': TOKEN } 
+    //     });
+    // }
+    // czyszczenie localStorage
+    localStorage.clear();
+    // przeladowanie zeby wrocic do "czystego" stanu
+    location.reload();
+}
+
 // pobieranie roslin
 async function loadPlants() {
-    const response = await fetch('/api/plants');
+    const response = await fetch('/api/plants', {
+        headers: { 'Authorization': TOKEN }
+    });
+
+    // jesli token wygasl to wylogowanie
+    if (response.status === 401) {
+        logout();
+        return;
+    }
     const plants = await response.json();
     
     const list = document.getElementById('plantsList');
@@ -13,13 +87,27 @@ async function loadPlants() {
         // style
         div.className = 'plant-item';
 
-        // nazwa i przycisk
+        // nazwa i przycisk do usuwania
         div.innerHTML = `
             <span>🌱 <b>${plant.name}</b> <small>(ID: ${plant.id})</small></span>
             <button class="delete-btn" onclick="deletePlant(${plant.id})"">Usuń</button>
         `;
         list.appendChild(div);
     });
+}
+
+// wyswietlanie aplikacji
+function showApp() {
+    // ukrycie logowania i wyswietlenie aplikacji po zalogowaniu uzytkownika
+    document.getElementById('loginView').style.display = 'none';
+    document.getElementById('appView').style.display = 'block';
+    
+    // dane uzytkownika
+    document.getElementById('currentUser').innerText = USERNAME;
+    document.getElementById('currentRole').innerText = ROLE;
+
+    // zaladowanie roslin
+    loadPlants();
 }
 
 // dodawanie roslin
@@ -31,7 +119,7 @@ async function addPlant() {
 
     await fetch('/api/plants', {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
+        headers: { 'Content-Type': 'application/json', 'Authorization': TOKEN },
         body: JSON.stringify({ name: name })
     });
 
@@ -46,12 +134,10 @@ async function deletePlant(id) {
 
     // wyslanie zadania DELETE do serwera
     await fetch(`/api/plants/${id}`, {
-        method: 'DELETE'
+        method: 'DELETE',
+        headers: { 'Authorization': TOKEN }
     });
 
     // odswiezenie listy po usunieciu
     loadPlants();
 }
-
-// zaladowanie roslin przy starcie
-loadPlants();
--- server/server.js
+++ server/server.js
@@ -2,9 +2,16 @@ const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 const bodyParser = require('body-parser');
 const path = require('path');
+const cors = require('cors');
+// do szyfrowania hasel
+const bcrypt = require('bcryptjs'); 
+// do generowania tokenow
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 const PORT = 3000;
+
+app.use(cors());
 
 // do obslugi json
 app.use(bodyParser.json());
@@ -18,40 +25,165 @@ const db = new sqlite3.Database(db_path, (err) => {
 });
 
 db.serialize(() => {
-    db.run("CREATE TABLE IF NOT EXISTS plants (id INTEGER PRIMARY KEY, name TEXT)");
+    // tabela UZYTKOWNIKOW
+    db.run(`CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY,
+        username TEXT UNIQUE,
+        password TEXT,
+        role TEXT
+    )`)
+
+    // dane admina sa w bazie, nie mozna sie zarejestrowac jako admin
+    const adminPassword = 'admin123';
+    const salt = bcrypt.genSaltSync(10);
+    const adminHash = bcrypt.hashSync(adminPassword, salt);
+
+    // ustawi takiego admina tylko jesli nie istnieje w bazie
+    db.run(`INSERT OR IGNORE INTO users (username, password, role) 
+            VALUES ('admin', ?, 'admin')`, [adminHash], (err) => {
+        if (!err) console.log("System: Konto Administratora gotowe (login: admin, hasło: admin123)");
+    });
+
+    // tabela ROSLIN
+    db.run(`CREATE TABLE IF NOT EXISTS plants (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        owner_id INTEGER
+    )`);
+
     // testowa roslina
+    db.run("INSERT OR IGNORE INTO plants (id, name) VALUES (1, 'Testowa Paprotka')");
+
     db.run("INSERT OR IGNORE INTO plants (id, name) VALUES (1, 'Testowa Paprotka')");
 });
 
+// PAMIEC SESJI
+// tokeny
+const sessions = {};
+
+const authenticate = (req, res, next) => {
+    const token = req.headers['authorization'];
+
+    if (!token || !sessions[token]) {
+        return res.status(401).json({ error: "Brak dostępu.Zaloguj się."});
+    }
+
+    req.user = sessions[token]
+    next();
+}
+
 // API
 
+// ENDPOINTY REJESTRACJI I LOGOWANIA
+
+// rejestracja
+app.post('/api/register', (req, res) => {
+    const { username, password } = req.body;
+    
+    if(!username || !password) return res.status(400).json({error: "Podaj login i hasło."});
+
+    // nie mozna sie zarejestrowac jako admin
+    if (username.toLowerCase() === 'admin') {
+        return res.status(400).json({error: "Nazwa 'admin' jest zarezerwowana."});
+    }
+
+    const hashedPassword = bcrypt.hashSync(password, 10);
+    
+    // zawsze rola user
+    const role = 'user'; 
+
+    // dodanie uzytkownika do bazy
+    db.run("INSERT INTO users (username, password, role) VALUES (?, ?, ?)", 
+        [username, hashedPassword, role], 
+        function(err) {
+            if (err) {
+                // blad login zajety
+                return res.status(400).json({error: "Użytkownik o takim loginie już istnieje."});
+            }
+            res.json({ success: true });
+        }
+    );
+});
+
+// logowanie
+app.post('/api/login', (req, res) => {
+    const { username, password } = req.body;
+
+    db.get("SELECT * FROM users WHERE username = ?", [username], (err, user) => {
+        if (!user) return res.status(400).json({error: "Błędny login lub hasło."});
+
+        // sprawdzenie haszowanego hasla
+        const passwordIsValid = bcrypt.compareSync(password, user.password);
+        if (!passwordIsValid) return res.status(400).json({error: "Błędny login lub hasło."});
+
+        // generowanie tokenu sesji
+        const token = uuidv4();
+        sessions[token] = { userId: user.id, role: user.role, username: user.username };
+
+        res.json({ token, role: user.role, username: user.username });
+    });
+});
+
+// wylogowanie
+app.post('/api/logout', (req, res) => {
+    const token = req.headers['authorization'];
+    if (token) delete sessions[token];
+    res.json({ success: true });
+});
+
+// API DLA ADMINA -> do zrobienia
+
+// ENDPOINTY ROSLIN
+
 // GET -> pobranie listy roslin
-app.get('/api/plants', (req, res) => {
-    db.all("SELECT * FROM PLANTS", [], (err, rows) => {
+app.get('/api/plants', authenticate, (req, res) => {
+    // admin widzi wszystkie a uzytkownik tylko swoje
+    let sql = "SELECT * FROM plants";
+    let params = [];
+
+    // dodanie warunku dla uzytkownika ze widzi tylko swoje rosliny
+    if (req.user.role !== 'admin') {
+        sql += " WHERE owner_id = ?";
+        params.push(req.user.userId);
+    }
+
+    db.all(sql, params, (err, rows) => {
         if (err) return res.status(500).json({error: err.message});
         res.json(rows);
     });
 });
 
 // POST - dodanie rosliny do listy
-app.post('/api/plants', (req, res) => {
+app.post('/api/plants', authenticate, (req, res) => {
     const { name } = req.body;
     if (!name) return res.status(400).json({error: "Nie podano nazwy."});
     
-    db.run("INSERT INTO plants (name) VALUES (?)", [name], function(err) {
-        if (err) return res.status(500).json({error: err.message});
-        res.json({ id: this.lastID, name: name });
-    });
+    // id zalogowanego to id wlasciciela
+    db.run("INSERT INTO plants (name, owner_id) VALUES (?, ?)", 
+        [name, req.user.userId], 
+        function(err) {
+            if (err) return res.status(500).json({error: err.message});
+            res.json({ id: this.lastID, name: name });
+        }
+    );
 });
 
 // DELETE - usuwanie rosliny po ID
-app.delete('/api/plants/:id', (req, res) => {
-    const id = req.params.id; // pobieramy ID z adresu URL
+app.delete('/api/plants/:id', authenticate, (req, res) => {
+    const id = req.params.id;
     
-    db.run("DELETE FROM plants WHERE id = ?", id, function(err) {
-        if (err) return res.status(500).json({error: err.message});
-        
-        res.json({ message: `Pomyślnie usunięto roślinę ID: ${id}.` });
+    // czyja roslina
+    db.get("SELECT owner_id FROM plants WHERE id = ?", [id], (err, plant) => {
+        if (!plant) return res.status(404).json({error: "Nie znaleziono rośliny"});
+
+        if (req.user.role !== 'admin' && plant.owner_id !== req.user.userId) {
+            return res.status(403).json({error: "Nie masz uprawnień do usunięcia tej rośliny!"});
+        }
+
+        db.run("DELETE FROM plants WHERE id = ?", id, function(err) {
+            if (err) return res.status(500).json({error: err.message});
+            res.json({ message: `Usunięto roślinę ID: ${id}` });
+        });
     });
 });
 
